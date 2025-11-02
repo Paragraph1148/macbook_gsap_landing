e95a5e87e1c603c5b3ed53d457b908e9d50f65a2
Paragraph1148 <actuallyamountain@proton.me>
2025-11-02 07:16:05 +0530

    fixed box 	modified:   src/components/ProductViewer.jsx 	deleted:    src/components/StudioLights.jsx 	src/components/three/

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacBookModel14 from "./models/Macbook-14";
-import StudioLights from "./StudioLights";
+import StudioLights from "./three/StudioLights";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacBookStore();
@@ -56,15 +56,15 @@ const ProductViewer = () => {
             </div>
           </div>
         </div>
-        <Canvas
-          id="canvas"
-          camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
-        >
-          <StudioLights />
-          <MacBookModel14 scale={0.06} position={[0, 0, 0]} />
-          <OrbitControls enableZoom={false} />
-        </Canvas>
       </div>
+      <Canvas
+        id="canvas"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <StudioLights />
+        <MacBookModel14 scale={0.06} position={[0, 0, 0]} />
+        <OrbitControls enableZoom={false} />
+      </Canvas>
     </section>
   );
 };
